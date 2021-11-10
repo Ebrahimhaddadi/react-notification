560f0597681f7f8870d9f8c11ce7ac402125bf17
Ebrahim Hadadi <hadadi.ebi@gmail.com>
2021-11-10 11:46:42 +0330

    add config onesignal notificatio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react'
+import OneSignal from 'react-onesignal';
+const App = () => {
+ 
 
-function App() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "8699bee1-37be-46cd-99e7-11493c023549"
+    });
+  }, []);
+  const onHandleTag=(tag)=>{
+OneSignal.sendTag('tech',tag).then(()=>{
+  console.log("Tagged")
+})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>push notification</h1>
+      <button onClick={()=>onHandleTag('react')}>react</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
